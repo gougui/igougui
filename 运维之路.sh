bea724c99cea60bd32062c56a77db6f8056ba3ba
gougui <1105576170@qq.com>
2019-09-26 16:35:42 +0800

    Create 运维之路.sh

---
+++ 运维之路.sh
@@ -0,0 +1,13 @@
+0 说明{
+
+    手册制作: 白凤
+    更新日期: 2019-09-26
+
+    请使用"notepad++"或其它编辑器打开此文档, "alt+0"将函数折叠后方便查阅
+    错误在所难免, 还望指正！
+
+    [python实例手册] [shell实例手册] [LazyManage运维批量管理(shell/python两个版本)]
+    网站更新下载地址:    https://www.igougui.com/
+    github更新下载地址:  https://github.com/gougui/igougui
+
+}
